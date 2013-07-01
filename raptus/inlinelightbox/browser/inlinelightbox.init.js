1f0b7cb0f2f1563f96173a96c547fd77978b8264
Simon Käser <skaeser@raptus.com>
2013-07-01 16:31:46 +0200

    Adjusted javascript initialization to work with the new component selection functionality introduced in raptus.article.core 2.0b13

--- raptus/inlinelightbox/browser/inlinelightbox.init.js
+++ raptus/inlinelightbox/browser/inlinelightbox.init.js
@@ -1,12 +1,21 @@
-jQuery(document).ready(function() {
-  var images = new Array();
-  jQuery('a[rel^="inlinelightbox"]').each(function() {
-    var o = jQuery(this);
-    var rel = o.attr('rel');
-    if(jQuery.inArray(rel, images) == -1)
-      images.push(rel);
-  });
-  for(var i=0; i<images.length; i++) {
-    jQuery('a[rel="'+images[i]+'"]').inlineLightBox(inlinelightbox.settings['standard']);
+(function($) {
+
+  function init(e) {
+    var container = $(this);
+    var images = new Array();
+    container.find('a[rel^="inlinelightbox"]').each(function() {
+      var o = $(this);
+      var rel = o.attr('rel');
+      if($.inArray(rel, images) == -1)
+        images.push(rel);
+    });
+    for(var i=0; i<images.length; i++)
+      container.find('a[rel="'+images[i]+'"]').inlineLightBox(inlinelightbox.settings['standard']);
   }
-});
+
+  $(document).ready(function(e) {
+    $.proxy(init, $('body'))(e);
+    $('.viewletmanager').on('viewlets.updated', init);
+  });
+
+})(jQuery);
