ee35942a3b85afd9ce53be462f0946359fc87c28
Simon Käser <skaeser@raptus.com>
2013-07-03 11:13:30 +0200

    Backwards compatibility for jQuery < 1.7

--- raptus/inlinelightbox/browser/inlinelightbox.init.js
+++ raptus/inlinelightbox/browser/inlinelightbox.init.js
@@ -15,7 +15,7 @@
 
   $(document).ready(function(e) {
     $.proxy(init, $('body'))(e);
-    $('.viewletmanager').on('viewlets.updated', init);
+    $('.viewletmanager').bind('viewlets.updated', init);
   });
 
 })(jQuery);
